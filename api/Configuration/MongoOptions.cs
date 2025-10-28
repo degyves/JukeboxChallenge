1481d83e414e43261c567a670b86bc5dd3221035
Victor de Gyves <degyves@lexmachina.com>
2025-10-28 09:54:11 -0600

    finished the hackaton

--- api/Configuration/MongoOptions.cs
+++ api/Configuration/MongoOptions.cs
@@ -4,7 +4,7 @@ public class MongoOptions
 {
     public const string SectionName = "Mongo";
 
-    public string ConnectionString { get; set; } = "mongodb://mongo:27017";
+    public string ConnectionString { get; set; } = "mongodb://mongodb:27017";
 
     public string Database { get; set; } = "partyjukebox";
 }
